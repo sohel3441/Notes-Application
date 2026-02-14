763d99173d8f9025fafe0de6b2abde2e3c922fb1
sohel3441 <shaikhsohel113441@gmail.com>
2026-02-14 12:17:15 +0530

    deploying

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,15 @@
+// import { io } from "socket.io-client";
+
+// export const socket = io(import.meta.env.VITE_SOCKET_URL);
+
+
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_SOCKET_URL);
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  transports: ["websocket"],   // force websocket
+  withCredentials: true,       // needed if using auth/cookies
+  autoConnect: true,           // optional, default true
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000
+});
